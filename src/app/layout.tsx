7341451f66ef4d022b39eba0e147559fe3d63f93
timwatts80 <tim_watts@ymail.com>
2025-08-14 16:43:08 -0600

    Add custom favicon with Tim signature on black background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Tim Watts Art - Links",
   description: "Links to Tim Watts' social media and art profiles",
+  icons: {
+    icon: '/favicon.svg',
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
